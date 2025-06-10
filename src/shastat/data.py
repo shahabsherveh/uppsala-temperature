008bf42179238820fcb90f9081c5dbe1c008861e
Shahab <shahabsherveh@gmail.com>
2025-06-10 07:25:29 +0200

    Adds data slicing

--- src/shastat/data.py
+++ src/shastat/data.py
@@ -1,23 +1,44 @@
 import pandas as pd
 import numpy as np
+from .utils import BaseConfig
 
 
-def read_data(file_path):
+class DataProcessingConfig(BaseConfig):
+    """
+    Configuration class for data processing.
+    """
+
+    def __init__(
+        self,
+        resample_freq: str = "ME",
+        start_year: str | None = None,
+        end_year: str | None = None,
+    ):
+        self.resample_freq = resample_freq
+        self.start_year = start_year
+        self.end_year = end_year
+
+
+def read_data(
+    file_path,
+    sep=r"\s+",
+    columns=["year", "month", "day", "temp", "temp_corrected", "data_source"],
+):
     """
     Read the data from the specified file path.
     """
     df = pd.read_csv(
         file_path,
-        sep=r"\s+",
+        sep=sep,
+        names=columns,
     )
-    df.columns = ["year", "month", "day", "temp", "temp_corrected", "data_source"]
     return df
 
 
-def preprocess_data(df, resample, test_size):
+def preprocess_data(
+    df, resample_freq, start_year: str | None = None, end_year: str | None = None
+):
     df["date"] = pd.to_datetime(df[["year", "month", "day"]])
     df.set_index("date", inplace=True)
-    ts = df.temp_corrected.resample(resample).mean()
-    train = ts[:test_size].copy()
-    test = ts[-test_size:].copy()
-    return train, test
+    ts = df.temp_corrected.resample(resample_freq).mean()
+    return ts[start_year:end_year]
